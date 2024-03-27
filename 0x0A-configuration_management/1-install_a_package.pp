d530cb0aae2e69e9163d4448504a7ed18dbd4a73
telmon95 <telmonm@gmail.com>
2024-03-27 16:02:10 -0700

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
-#install specific version of flask (2.1.0)
+# Install an especific version of flask (2.1.0)
 package {'flask':
-	ensure => '2.1.0',
-	provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
+
