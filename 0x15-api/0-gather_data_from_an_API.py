2bd417c7c485ee7c901ecb9f03f24262ab784809
telmon95 <telmonm@gmail.com>
2024-04-22 15:18:40 -0700

    correction 2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
+"""Script to retrieve TODO list progress of a given employee from REST API."""
+import requests
 import sys
 
-if __name__ == '__main__':
+def get_employee_info(user_id):
+    """Retrieve user information from the API."""
     url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    response = requests.get(url + 'users/{}'.format(user_id))
+    if response.status_code != 200:
+        print("Error: Failed to retrieve user information.")
+        sys.exit(1)
+    return response.json()
+
+def get_todo_list(user_id):
+    """Retrieve TODO list for the given user ID from the API."""
+    url = 'https://jsonplaceholder.typicode.com/'
+    response = requests.get(url + 'todos', params={'userId': user_id})
+    if response.status_code != 200:
+        print("Error: Failed to retrieve TODO list.")
+        sys.exit(1)
+    return response.json()
+
+def main():
+    """Main function to fetch and display employee's TODO list progress."""
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    user_info = get_employee_info(employee_id)
+    todo_list = get_todo_list(employee_id)
+
+    completed_tasks = [task['title'] for task in todo_list if task['completed']]
+    total_tasks = len(todo_list)
+
+    print("Employee {} is done with tasks ({}/{}):".format(
+        user_info['name'], len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t", task)
+
+if __name__ == '__main__':
+    main()
+
