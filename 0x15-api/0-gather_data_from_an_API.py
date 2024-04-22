a47602ff1af5b020af5107ea0faaceaf43eccc69
telmon95 <telmonm@gmail.com>
2024-04-22 15:11:33 -0700

    re-commoit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,3 +16,4 @@ if __name__ == '__main__':
                                                           len(completed),
                                                           len(to_do)))
     [print("\t {}".format(title)) for title in completed]
+
