161c5b415c3d6f54b5f5027c9b0baacd841e1f22
telmon95 <telmonm@gmail.com>
2024-05-08 18:09:19 -0700

    another commit

--- 0x16-api_advanced/count_subs.py
+++ 0x16-api_advanced/count_subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
         Gecko/20100101 Firefox/73.0"
     }
 
-    response = requests.get(url, headers=header, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     return response.json().get('data').get('subscribers')
