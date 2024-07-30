f0873ecfd4d142170499db5cebc9d0fcc18354e2
telmon95 <telmonm@gmail.com>
2024-07-30 00:52:26 +0000

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 
+
 def get_employee_info(user_id):
     """Retrieve user information from the API."""
     url = 'https://jsonplaceholder.typicode.com/'
@@ -11,6 +12,7 @@ def get_employee_info(user_id):
         print("Error: Failed to retrieve user information.")
         sys.exit(1)
     return response.json()
+
 
 def get_todo_list(user_id):
     """Retrieve TODO list for the given user ID from the API."""
@@ -21,23 +23,22 @@ def get_todo_list(user_id):
         sys.exit(1)
     return response.json()
 
+
 def main():
     """Main function to fetch and display employee's TODO list progress."""
     if len(sys.argv) != 2:
         print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
-
     employee_id = sys.argv[1]
     user_info = get_employee_info(employee_id)
     todo_list = get_todo_list(employee_id)
-
-    completed_tasks = [task['title'] for task in todo_list if task['completed']]
+    completed_tasks = [task for task in todo_list if task['completed']]
     total_tasks = len(todo_list)
-
-    print("Employee {} is done with tasks ({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user_info['name'], len(completed_tasks), total_tasks))
     for task in completed_tasks:
-        print("\t {}".format(task))
+        print("\t {}".format(task['title']))
+
 
 if __name__ == '__main__':
     main()
