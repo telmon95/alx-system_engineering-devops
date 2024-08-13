168dde800b1190eb6508f28b5e1b30e5bf6e5a8b
telmon95 <telmonm@gmail.com>
2024-08-13 23:15:16 +0200

    correction 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,11 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0-subs:v1.0.0 (by /u/your_reddit_username)"
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        results = response.json().get("data", {})
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
         return results.get("subscribers", 0)
-    except requests.RequestException:
-        return 0
+    return 0
+    
